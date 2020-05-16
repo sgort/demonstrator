0fdbf6f86db02e12a01f10728f7c41a9c748c51e
Datafluisteraar <steven.gort@ictu.nl>
2020-05-16 12:04:19 +0200

    added images

--- src/home.js
+++ src/home.js
@@ -5,14 +5,9 @@ class Home extends Component {
         return (
             <div>
                 <h2>HOME</h2>
-                <p>Cras facilisis urna ornare ex volutpat, et
-                convallis erat elementum. Ut aliquam, ipsum vitae
-                gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                metus nec massa. Maecenas hendrerit laoreet augue
-                nec molestie. Cum sociis natoque penatibus et magnis
-                dis parturient montes, nascetur ridiculus mus.</p>
-
-                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+                <p>
+                    <img src={require('./images/discipl-api-strategy.jpg')} alt="" />
+                </p>
             </div>
         );
     }
